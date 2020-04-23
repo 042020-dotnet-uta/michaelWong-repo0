1d8b9b897b130fda0ab00037e3eb7304b2ef4d94
michwong98 <michwong98@gmail.com>
2020-04-23 12:04:22 -0400

    Experiments with delegates

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -2,11 +2,35 @@
 
 namespace Hello
 {
+
+    public delegate int Del(String message);
+    
     class Program
     {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Del print = (String message) =>
+            {
+                Console.WriteLine("1: " + message);
+                return 0;
+            };
+
+            Del printMore = (String message) =>
+            {
+                Console.WriteLine("2: I am the delegate.");
+                return 1;
+            };
+
+            Del printing = (String message) =>
+            {
+                Console.WriteLine("3: I am printing.");
+                return 2;
+            };
+
+            Del allDelegates = print + printMore + printing;
+            Console.WriteLine(allDelegates);
+            
         }
     }
 }
