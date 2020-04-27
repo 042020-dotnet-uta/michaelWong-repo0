9fc00879bf8d70f4798ebae298b8f885e64ce183
michwong98 <michwong98@gmail.com>
2020-04-27 11:32:26 -0400

    Creates CodingChallenge1

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,22 +1,6 @@
 ﻿using System;
-using System.Text.RegularExpressions;
-
-namespace Hello
-{
-    
-    class Program
-    {
-
-        public static void Main(String[] args)
-        {
-
-            Regex rx = new Regex(@"^[\p{Lu}\p{Ll}\p{Nd}]{8,}$");
-            System.Console.WriteLine(rx.IsMatch("faoiasdj wfafasa"));
-            System.Console.WriteLine(rx.IsMatch("W11h81^(21"));
-            System.Console.WriteLine(rx.IsMatch("_"));
-            System.Console.WriteLine(rx.IsMatch(""));
+namespace Hello {     
+    class Program {
         }
-
     }
-
 }
